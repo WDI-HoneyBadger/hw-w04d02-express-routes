6c2d6005ea176ea6584b53a49735ffb5b1a6d8cc
hamoudsa <halqahta2007@my.fit.edu>
2018-12-03 18:13:33 +0300

    done

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+var definition = require('./definititons/definitions');
+ app.get('/', function(request, response){
+    response.send('welcome to the terms dictionary!!')
+}) 
+app.get('/definitions/:definition', function(request, response){
+    var definitions = request.params.definition;
+    var termDeifintion = definition[definitions];
+    response.send(`${definitions} is ${termDeifintion}`);
+}) 
+ app.listen(port, function(){
+    console.log('welcome to the terms dictionary!!')
+}) 
