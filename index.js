8bfe022dacd7e79e72a211721ef75efb880dcfb8
khalidnt <405795+khalidnt@users.noreply.github.com>
2018-12-03 22:59:00 +0300

    w0402

--- index.js
+++ index.js
@@ -3,3 +3,19 @@ var express = express();
 var port = 3000;
 
 
+app.listen(port, function(){
+    console.log(`App is listening on port ${port}`);
+})
+
+var definitions = require('./definitions');
+
+app.get('/', function(request, response){
+    response.send('Welcome To The Terms Dictionary!!');
+})
+
+var term = request.params.definitions;
+var definition = definitions[definition]
+
+app.get('/:definitions', function(request, response){
+    response.send(`the definition of ${term} is ${definition}`);
+}) 
