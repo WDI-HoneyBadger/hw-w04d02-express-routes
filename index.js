f82e14a22840db228cad9ff98385e4da8f280f0a
Mohammed Alghamdi <44502915+MohammedAlghamdi3@users.noreply.github.com>
2018-12-04 01:36:00 +0300

    HW4
    completeness: 5/5
    comfort: 5/5

---
+++ index.js
@@ -0,0 +1,24 @@
+// Set up express
+var express = require('express');
+var app = express();
+var port = 3000;
+
+// homepage
+app.get('/', function(request, response){
+    response.send('This is Mohammed !!!');
+});
+
+ // mport definitions
+    var definitions = require('./definitions');
+    
+app.get('/:definitions', function(request,response){
+ var definitionstName= request.params.definitions;
+var definitionsvalue = definitions[definitionstName];
+
+  response.send(`${definitionsvalue} `)
+});
+
+// app listen
+app.listen(port, function(){
+console.log(`App is listening on port ${port}`);
+});
