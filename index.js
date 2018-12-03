d0444b21affafa25132b210229dfeaebf71cffb8
NawafI <nawaf.alhathni@gmail.com>
2018-12-03 18:29:51 +0300

    hw-w04d02-express-routes

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+var definitions = require('./definitions');
+
+app.get('/', function(request, response){
+    response.send('welcome to the terms dictionary!!')
+
+})
+
+app.get('/:terms', function(request, response){
+    var sentence = request.params.terms;
+     var defi = definitions[sentence]
+     response.send(`${sentence}: ${defi} `);
+})
+
+
+
+
+
+
+
+
+
+
+app.listen(port, function(){
+    console.log(`"welcome to the terms dictionary!!". ${port}`)
+})
